94c70a2ac83878c57f1d5e576d9c1d67cf65741d
Harry Muriithi <hharryjjack@gmail.com>
2023-09-19 15:32:17 +0300

    Update Place: (models/place.py)

--- models/city.py
+++ models/city.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
+    __tablename__ = 'city'
+
     state_id = ""
-    name = ""
+    name = Column(String(128), nullable=False)
+
+    places = relationship(
+            "Place",
+            back_populates="city",
+            cascade="all, delete-orphan")

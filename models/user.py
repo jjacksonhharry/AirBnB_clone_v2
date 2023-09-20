bceb701a932a195e4b3e028512f8487a0150e3d4
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-09-21 02:05:09 +0300

    Update User class to accept db storage

--- models/user.py
+++ models/user.py
@@ -8,7 +8,7 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'users'
@@ -16,6 +16,7 @@ class User(BaseModel):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=False)
         last_name = Column(String(128), nullable=False)
+        """
         places = relationship(
                 "Place",
                 backref="user",
@@ -24,6 +25,7 @@ class User(BaseModel):
                 "Review",
                 backref="user",
                 cascade="all, delete-orphan")
+        """
 
     else:
         email = ""

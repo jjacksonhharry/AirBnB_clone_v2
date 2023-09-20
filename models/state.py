de13c64c919ddbd2098c42c448ae463da2614661
Harry Muriithi <hharryjjack@gmail.com>
2023-09-20 18:15:36 +0300

    DBStorage - States and Cities

--- models/state.py
+++ models/state.py
@@ -1,35 +1,30 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from models import storage
-from os import getenv
-from models.city import City
+import os
 
 
-class State(BaseModel, Base):
+class State(BaseModel):
     """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        # for dbstorage
+    if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
         cities = relationship(
                 'City',
                 backref='state',
                 cascade='all, delete-orphan')
-
     else:
         name = ""
 
-    # for file storage
-    @property
-    def cities(self):
-        """
-        Getter attribute for cities
-        """
-        city_list = []
-        for city in storage.all(City).values():
-            if city.state_id == self.id:
-                city_list.append(city)
-        return city_list
+    if os.environ.get('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """Getter for cities in FileStorage"""
+            from models import storage
+            city_list = []
+            for city in storage.all("City").values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list

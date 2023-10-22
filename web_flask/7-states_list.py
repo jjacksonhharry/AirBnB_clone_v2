5eac3d245c89913b26bf88a785124e55bd2d20f4
Harry Muriithi <hharryjjack@gmail.com>
2023-10-22 21:37:29 +0300

    Write a script that starts a Flask web application

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,22 +1,31 @@
 #!/usr/bin/python3
+"""
+script that starts a Flask web application
+"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-app = Flask(__name)
+
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def display_states():
+    """
+    Render state_list html page to display States created
+    """
+    states = storage.all()
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown(exception):
+def teardown(self):
+    """
+    Method to remove current SQLAlchemy Session
+    """
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    states = storage.all(State).values()
-    states_sorted = sorted(states, key=lambda state: state.name)
-
-    return render_template('7-states_list.html', states=states_sorted)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
